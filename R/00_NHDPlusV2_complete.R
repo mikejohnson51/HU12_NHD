02d580dd91c2ff4777aae22f8793d66b407d9224
David Blodgett <dblodgett@usgs.gov>
2019-03-31 20:18:12 -0500

    rename and tidy

--- R/00_NHDPlusV2_complete.R
+++ R/00_NHDPlusV2_complete.R
@@ -3,10 +3,11 @@ library(sf)
 library(dplyr)
 library(HUCAgg)
 
-# natdb <- "data/nhdplus/NHDPlusNationalData/NHDPlusV21_National_Seamless.gdb/"
-# wbd <- read_sf(natdb, "HUC12")
-# wbd <- select(wbd, HUC12 = HUC_12, TOHUC = HU_12_DS)
+natdb <- "data/nhdplus/NHDPlusNationalData/NHDPlusV21_National_Seamless.gdb/"
+wbd <- read_sf(natdb, "HUC12")
+wbd <- select(wbd, HUC12 = HUC_12, TOHUC = HU_12_DS)
 
+##### Fixes #####
 fixes <- tibble(HUC12 = character(0),
                 TOHUC = character(0),
                 comment = character(0)) %>%
@@ -484,61 +485,62 @@ fixes <- tibble(HUC12 = character(0),
                  comment =  "UNKNOWN fix")) %>%
   bind_rows(list(HUC12 = "090100032207", TOHUC = "090100030000",
                  comment =  "UNKNOWN fix"))
+##### Setup #####
 
+wbd <- filter(wbd, !grepl("^20.*|^19.*|^21.*|^22.*", wbd$HUC12))
 
+# # Check if what we have is a DAG.
+for(fix in 1:nrow(fixes)) {
+  wbd$TOHUC[wbd$HUC12 == fixes$HUC12[fix]] <- fixes$TOHUC[fix]
+  if(!igraph::is.dag(igraph::graph_from_data_frame(st_set_geometry(wbd, NULL)))) message(fix)
+}
+wbd <- st_transform(wbd, 5070)
+write_sf(wbd, "wbd_viz.gpkg", "wbd_viz")
 
-# wbd <- filter(wbd, !grepl("^20.*|^19.*|^21.*|^22.*", wbd$HUC12))
-# 
-# # # Check if what we have is a DAG.
-# for(fix in 1:nrow(fixes)) {
-#   wbd$TOHUC[wbd$HUC12 == fixes$HUC12[fix]] <- fixes$TOHUC[fix]
-#   if(!igraph::is.dag(igraph::graph_from_data_frame(st_set_geometry(wbd, NULL)))) message(fix)
-# }
-# wbd <- st_transform(wbd, 5070)
-# write_sf(wbd, "wbd_viz.gpkg", "wbd_viz")
-# 
-# net <- read_sf(natdb, "NHDFlowline_Network") %>%
-#   st_zm()
-# 
-# net_prep <- prepare_nhdplus(net, 0, 0, TRUE, TRUE) %>%
-#   left_join(select(net, COMID, DnLevelPat, AreaSqKM)) %>%
-#   st_sf() %>%
-#   group_by(LevelPathI) %>%
-#   arrange(Hydroseq) %>%
-#   mutate(DnLevelPat = DnLevelPat[1]) %>%
-#   ungroup()
-# 
-# net_prep["denTotalAreaSqKM"] <-
-#   calculate_total_drainage_area(select(st_set_geometry(net_prep, NULL),
-#                                        ID = COMID, toID = toCOMID,
-#                                        area = AreaSqKM))
-# 
-# net_atts <- st_set_geometry(net, NULL)[ ,1:40]
-# rm(net)
-# 
-# net_prep <- st_transform(net_prep, 5070)
-# 
-# net_prep <- st_simplify(net_prep, 10)
-# wbd <- st_simplify(wbd, 10)
-# 
-# net_prep <- st_join(net_prep, select(wbd, HUC12, TOHUC)) %>%
-#   st_set_geometry(NULL)
-# saveRDS(net_prep, "net_prep_cache_nhdp.rds")
-# 
-# net_prep <- readRDS("net_prep_cache_nhdp.rds")
-# 
-# wbd_atts <- st_set_geometry(wbd, NULL)
-# 
-# rm(wbd)
-# rm(fix)
-# rm(fixes)
-# rm(natdb)
-# 
-# save(net_atts, net_prep, wbd_atts, file = "nhd_wbd.RData")
-# 
-# rm(net_atts)
-# rm(net_prep)
-# rm(wbd_atts)
+net <- read_sf(natdb, "NHDFlowline_Network") %>%
+  st_zm()
+
+net_prep <- prepare_nhdplus(net, 0, 0, TRUE, TRUE) %>%
+  left_join(select(net, COMID, DnLevelPat, AreaSqKM)) %>%
+  st_sf() %>%
+  group_by(LevelPathI) %>%
+  arrange(Hydroseq) %>%
+  mutate(DnLevelPat = DnLevelPat[1]) %>%
+  ungroup()
+
+net_prep["denTotalAreaSqKM"] <-
+  calculate_total_drainage_area(select(st_set_geometry(net_prep, NULL),
+                                       ID = COMID, toID = toCOMID,
+                                       area = AreaSqKM))
+
+net_atts <- st_set_geometry(net, NULL)[ ,1:40]
+rm(net)
+
+net_prep <- st_transform(net_prep, 5070)
+
+net_prep <- st_simplify(net_prep, 10)
+wbd <- st_simplify(wbd, 10)
+
+net_prep <- st_join(net_prep, select(wbd, HUC12, TOHUC)) %>%
+  st_set_geometry(NULL)
+saveRDS(net_prep, "net_prep_cache_nhdp.rds")
+
+net_prep <- readRDS("net_prep_cache_nhdp.rds")
+
+wbd_atts <- st_set_geometry(wbd, NULL)
+
+rm(wbd)
+rm(fix)
+rm(fixes)
+rm(natdb)
+
+save(net_atts, net_prep, wbd_atts, file = "nhd_wbd.RData")
+
+rm(net_atts)
+rm(net_prep)
+rm(wbd_atts)
+
+##### Process #####
 
 load("nhd_wbd.RData")
 
@@ -601,11 +603,13 @@ all_GNIS_outlets <- parLapply(cl, to_run, par_fun,
                               net_prep = net_prep,
                               wbd_atts = wbd_atts)
 
-all_GNIS_outlets <- lapply(to_run, par_fun,
-                           net_atts = net_atts,
-                           net_prep = net_prep,
-                           wbd_atts = wbd_atts)
+# all_GNIS_outlets <- lapply(to_run, par_fun,
+#                            net_atts = net_atts,
+#                            net_prep = net_prep,
+#                            wbd_atts = wbd_atts)
 stopCluster(cl)
+
+##### Finalize #####
 
 out_files <- list.files("out", full.names = TRUE)
 
